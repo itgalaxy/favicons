f15881ddb82583fbcd347295bee4fcba5d966981
Hayden Bleasel <haydenbleasel@gmail.com>
2014-11-15 15:15:30 +1100

    Added Apple Startup Images task
    Issues: Broken sizes (outputs squares), logo padding inside images and
    elements aren’t written to HTML yet.

--- index.js
+++ index.js
@@ -9,7 +9,6 @@
         exec = require('child_process').exec,
         async = require('async'),
         defaults = require('lodash.defaults'),
-        sizeOf = require('image-size'),
         mkdirp = require('mkdirp'),
         rfg = require('real-favicon');
 
@@ -24,7 +23,8 @@
 
             // Icon Types
             android: true,
-            apple: true,
+            appleTouch: true,
+            appleStartup: true,
             coast: true,
             favicons: true,
             firefox: true,
@@ -55,10 +55,6 @@
         // Determine whether HTML is to be produced
         function writeHTML() {
             return options.html && options.html !== '';
-        }
-
-        function getSize(source) {
-            return path.extname(source) === '.svg' ? 1500 : sizeOf(source).width;
         }
 
         // Create a filepath.
@@ -113,17 +109,81 @@
             return image;
         }
 
+        /*<!-- iOS 6 & 7 iPad (retina, portrait) -->
+        <link href="/static/images/apple-touch-startup-image-1536x2008.png"
+              media="(device-width: 768px) and (device-height: 1024px)
+                 and (orientation: portrait)
+                 and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image">
+
+        <!-- iOS 6 & 7 iPad (retina, landscape) -->
+        <link href="/static/images/apple-touch-startup-image-1496x2048.png"
+              media="(device-width: 768px) and (device-height: 1024px)
+                 and (orientation: landscape)
+                 and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image">
+
+        <!-- iOS 6 iPad (portrait) -->
+        <link href="/static/images/apple-touch-startup-image-768x1004.png"
+              media="(device-width: 768px) and (device-height: 1024px)
+                 and (orientation: portrait)
+                 and (-webkit-device-pixel-ratio: 1)"
+              rel="apple-touch-startup-image">
+
+        <!-- iOS 6 iPad (landscape) -->
+        <link href="/static/images/apple-touch-startup-image-748x1024.png"
+              media="(device-width: 768px) and (device-height: 1024px)
+                 and (orientation: landscape)
+                 and (-webkit-device-pixel-ratio: 1)"
+              rel="apple-touch-startup-image">
+
+        <!-- iOS 6 & 7 iPhone 5 -->
+        <link href="/static/images/apple-touch-startup-image-640x1096.png"
+              media="(device-width: 320px) and (device-height: 568px)
+                 and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image">
+
+        <!-- iOS 6 & 7 iPhone (retina) -->
+        <link href="/static/images/apple-touch-startup-image-640x920.png"
+              media="(device-width: 320px) and (device-height: 480px)
+                 and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image">
+
+        <!-- iOS 6 iPhone -->
+        <link href="/static/images/apple-touch-startup-image-320x460.png"
+              media="(device-width: 320px) and (device-height: 480px)
+                 and (-webkit-device-pixel-ratio: 1)"
+              rel="apple-touch-startup-image">*/
+
+        // Make Apple Startup Images
+        function makeAppleStartup(callback) {
+            async.each(['1536x2008', '1496x2048', '768x1004', '748x1024', '640x1096', '640x920', '320x460'], function (size, callback) {
+                var name = 'apple-touch-startup-image-' + size + '.png',
+                    ratio = (size === '640x920' || size === '640x1096' || size === '1496x2048' || size === '1536x2008' ? 2 : 1),
+                    media = '(device-width: ' + size.substr(0, size.indexOf('x')) + ') and (device-height: ' + size.substr(size.indexOf('x'), size.length - 1) + ')',
+                    command = combine(whichImage(320), options.dest, size, name, opts);
+                convert(command, name, function () {
+                    elements.push('<link href="' + filePath(name) + '" media="' + media + ' and (-webkit-device-pixel-ratio: ' + ratio + ')" rel="apple-touch-startup-image" />');
+                    return callback();
+                });
+            }, function (error) {
+                if (error) {
+                    throw error;
+                }
+                return callback();
+            });
+        }
 
         // Create the appropriate icons
         function makeIcons(callback) {
             async.parallel([
-                /*function (callback) {
-                    if (options.opengraph) {
-                        makeAppleSplash(function () {
+                function (callback) {
+                    if (options.appleStartup) {
+                        makeAppleStartup(function () {
                             callback(null);
                         });
                     }
-                }*/
+                }
             ], function () {
                 return callback();
             });
@@ -147,7 +207,7 @@
         function design(callback) {
             var settings = {};
 
-            if (options.apple) {
+            if (options.appleTouch) {
                 settings.apple = {
                     picture_aspect: 'background_and_margin',
                     background_color: options.background,
