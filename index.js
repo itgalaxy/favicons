33f89ec59dc82979c25131790339d36f19931679
Matt Gaunt <matt@gauntface.co.uk>
2015-01-13 18:15:30 +0000

    Adding comments, variable changes, coding standards changes and error messages added in

--- index.js
+++ index.js
@@ -1,97 +1,134 @@
 /*jslint node:true, nomen:true, unparam:true*/
 
-(function () {
+(function() {
 
-    'use strict';
+  'use strict';
 
-    var _ = require('underscore'),
-        mergeDefaults = require('merge-defaults'),
-        through2 = require('through2'),
-        fs = require('fs'),
-        cheerio = require('cheerio'),
-        util = require('gulp-util'),
-        path = require('path'),
-        favicons = require('favicons');
+  var _ = require('underscore');
+  var mergeDefaults = require('merge-defaults');
+  var through2 = require('through2');
+  var fs = require('fs');
+  var cheerio = require('cheerio');
+  var util = require('gulp-util');
+  var path = require('path');
+  var favicons = require('favicons');
 
-    module.exports = function (params) {
+  function findInfo(source, callback) {
+    // Read in the source HTML file
+    fs.readFile(source, function(error, data) {
+      var $;
+      var info = {};
 
-        function findInfo(source, callback) {
-            fs.readFile(source, function (error, data) {
-                var $, info = {};
-                if (error) {
-                    throw error;
-                }
-                $ = cheerio.load(data);
-                info.favicon = $('link[rel="favicons"]').attr('href');
-                info.url = $('link[rel="canonical"]').attr('href');
-                info.title = $('title').text();
-                info.description = $('meta[name="description"]').attr('content');
-                info.author = $('meta[name="author"]').attr('content');
-                return callback(info);
-            });
+      if (error) {
+        throw error;
+      }
+
+      // Cheerio is a jquery core for node
+      $ = cheerio.load(data);
+
+      // Populate info with the appropriate data from the
+      // HTML page
+      info.favicon = $('link[rel="favicons"]').attr('href');
+      info.url = $('link[rel="canonical"]').attr('href');
+      info.title = $('title').text();
+      info.description = $('meta[name="description"]').attr('content');
+      info.author = $('meta[name="author"]').attr('content');
+
+      return callback(info);
+    });
+  }
+
+  module.exports = function(params) {
+
+    // Extend lodash with mergeDefaults
+    _.mixin({
+      'mergeDefaults': mergeDefaults
+    });
+
+    return through2.obj(function(file, enc, cb) {
+      // file.path will be the input file
+      findInfo(file.path, function(info) {
+        var faviconImageSrc = null;
+        if (params.files.src) {
+          // Use file.cwd here since the path is relative to the gulp file
+          faviconImageSrc = path.join(path.dirname(file.path), params.files.src);
+        } else if (info && info.favicon) {
+          // Use file.path here since the path is relative to the html page
+          // for link[rel="favicons"]
+          faviconImageSrc = path.join(path.dirname(file.path), info.favicon);
         }
 
-        _.mixin({ 'mergeDefaults': mergeDefaults });
+        // We need favicon
+        if (!faviconImageSrc) {
+          throw new Error('No link[rel="favicons"] defined OR files.src defined');
+        }
 
-        return through2.obj(function (file, enc, cb) {
-
-            findInfo(file.path, function (info) {
-
-                var options = _.mergeDefaults(params || {}, {
-                    files: {
-                        src: info ? (info.favicon ? path.join(path.dirname(file.path), info.favicon) : null) : null,
-                        dest: params.dest,
-                        html: file.path,
-                        iconsPath: null,
-                        androidManifest: null,
-                        browserConfig: null,
-                        firefoxManifest: null,
-                        yandexManifest: null
-                    },
-                    icons: {
-                        android: true,
-                        appleIcon: true,
-                        appleStartup: true,
-                        coast: true,
-                        favicons: true,
-                        firefox: true,
-                        opengraph: true,
-                        windows: true,
-                        yandex: true
-                    },
-                    settings: {
-                        appName: info.title,
-                        appDescription: info.description,
-                        developer: info.author,
-                        developerURL: info ? (info.url ? path.join(info.url, '/') : null) : null,
-                        background: null,
-                        index: null,
-                        url: info ? (info.url ? path.join(info.url, '/') : null) : null,
-                        logging: false
-                    }
-                });
-
-                if (file.isNull()) {
-                    cb(null, file);
-                    return;
-                }
-
-                if (file.isStream()) {
-                    cb(new util.PluginError('gulp-favicons', 'Streaming not supported'));
-                    return;
-                }
-
-                options.files.dest = path.join(path.dirname(file.path), options.files.dest);
-
-                favicons(options, function (error, html) {
-                    file.contents = new Buffer(_.flatten(html).join(' '));
-                    return cb(error, file);
-                });
-
-            });
-
+        // Merge a set of default params with our supplied
+        // config
+        var options = _.mergeDefaults(params || {}, {
+          files: {
+            src: faviconImageSrc,
+            dest: params.dest,
+            html: file.path,
+            iconsPath: null,
+            androidManifest: null,
+            browserConfig: null,
+            firefoxManifest: null,
+            yandexManifest: null
+          },
+          icons: {
+            android: true,
+            appleIcon: true,
+            appleStartup: true,
+            coast: true,
+            favicons: true,
+            firefox: true,
+            opengraph: true,
+            windows: true,
+            yandex: true
+          },
+          settings: {
+            appName: info.title,
+            appDescription: info.description,
+            developer: info.author,
+            developerURL: info ? (info.url ? path.join(info.url, '/') : null) : null,
+            background: null,
+            index: null,
+            url: info ? (info.url ? path.join(info.url, '/') : null) : null,
+            logging: false
+          }
         });
 
-    };
+        if (!fs.existsSync(options.files.src)) {
+          throw new Error('The favicon image doesn\'t exist: ' + options.files.src);
+        }
+
+        if (file.isNull()) {
+          cb(null, file);
+          return;
+        }
+
+        if (file.isStream()) {
+          cb(new util.PluginError('gulp-favicons', 'Streaming not supported'));
+          return;
+        }
+
+        // Get full path for destination of assets
+        options.files.dest = path.join(file.cwd, options.files.dest);
+
+        favicons(options, function(error, html) {
+          if (error) {
+            throw error;
+          }
+
+          file.contents = new Buffer(_.flatten(html).join(' '));
+          return cb(error, file);
+        });
+
+      });
+
+    });
+
+  };
 
 }());
