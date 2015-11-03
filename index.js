f210c97a1dc49c3c4332001badadfa805817c7ca
Hayden Bleasel <haydenbleasel@gmail.com>
2015-11-02 23:04:50 -0800

    Basic favicons generation using JIMP

--- index.js
+++ index.js
@@ -5,40 +5,98 @@
 
     // Node modules
     var _ = require('underscore'),
-        defaults = require('data/defaults.json');
+        async = require('async'),
+        Jimp = require('jimp'),
+        color = require('tinycolor2'),
+
+        // Configuration files
+        icons = require('./data/icons.json'),
+        defaults = require('./data/defaults.json');
 
     module.exports = function (source, configuration, callback) {
 
         // 1. Take a set of icon parameters (default all to true)
-        var options = _.defaults(configuration || {}, defaults);
+        var options = _.defaults(configuration || {}, defaults),
+            rgba = color(options.background).toRgb(),
+            background = Jimp.rgbaToInt(rgba.r, rgba.g, rgba.b, rgba.a * 255);
 
         function print(message) {
-            if (options.settings.logging && message) {
+            if (options.logging && message) {
                 console.log(message + '...');
             }
+        }
+
+        function createFile(config, name, callback) {
+            print('Generating file: ' + name);
+            return callback();
+        }
+
+        function createImage(config, name, callback) {
+            var minimum = Math.min(config.width, config.height),
+                offset = {
+                    height: (config.height - minimum > 0 ? (config.height - minimum) / 2 : 0),
+                    width: (config.width - minimum > 0 ? (config.width - minimum) / 2 : 0)
+                },
+                jimp = new Jimp(config.width, config.height, background, function (error, canvas) {
+                    if (error) {
+                        return callback(error);
+                    }
+                    Jimp.read(source).then(function (image) {
+                        image.resize(minimum, Jimp.AUTO);
+                        canvas.composite(image, offset.width, offset.height);
+                        image.getBuffer(Jimp.MIME_PNG, function (image, error, buffer) {
+                            return callback(error, { name: name, buffer: buffer });
+                        });
+                    }).catch(function (error) {
+                        return callback(error);
+                    });
+                });
+        }
+
+        function generateFavicons(files, callback) {
+            var html = [];
+            async.forEachOf(files, function (config, name, callback) {
+                html.push(config.html || null);
+                if (config.type === 'file') {
+                    createFile(config, name, function () {
+                        return callback(null);
+                    });
+                } else if (config.type === 'image') {
+                    createImage(config, name, function () {
+                        return callback(null);
+                    });
+                } else {
+                    return callback('Something went wrong with icon configuration.');
+                }
+            }, function (error) {
+                return callback(error, _.compact(html));
+            });
+        }
+
+        function createFavicons(platforms) {
+            async.forEachOf(platforms, function (enabled, platform, callback) {
+                if (enabled) {
+                    print('Generating favicons for platform: ' + platform);
+                    generateFavicons(icons[platform], function (error, images, html) {
+                        return callback(error, images, html);
+                    });
+                } else {
+                    return callback(null);
+                }
+            }, function (error) {
+                return callback(error);
+            });
         }
 
         // 2. Take a single image or an object of images { size: image }
         print('Favicons source is ' + (typeof source === 'object' ? 'an object' : 'a string') + '.');
 
-        // 3. Resize each image into it's appropriate size (solve object->resize logic)
-        if (typeof source === 'object') {
+        // 3. Resize each image and generate code (solve object->resize logic)
+        createFavicons(options.icons, function (error) {
 
-        } else {
-
-        }
-
-        /*
-            async.each(icons, function (platform) {
-                // Platform: Apple, Android, Coast, etc.
-
-            });
-        */
-
-        // 4. Generate code for each favicon
-
-        // 5. Return code and images into a buffer? Promise? Whatever. { code: '', images: { ... } }
-        return callback();
+            // 4. Return code and images into a buffer? Promise? Whatever. { code: '', images: { ... } }
+            return callback(error);
+        });
 
     };
 
--- test.js
+++ test.js
@@ -1,54 +1,9 @@
 /*jslint node:true*/
-var fs = require('fs'),
-    fav = require('./index.js'),
-    through = require('through2'),
-    File = require('vinyl'),
-  config = {
-    files: {
-        //src: 'test/logo.png',
-        src: {
-            "android": 'test/logo.png',
-            "appleIcon": 'test/logo.png',
-            "appleStartup": 'test/logo.png',
-            "coast": 'test/logo.png',
-            "favicons": 'test/logo.png',
-            "firefox": 'test/logo.png',
-            "opengraph": 'test/logo.png',
-            "windows": 'test/logo.png',
-            "yandex": 'test/logo.png'
-        },
-        dest: 'test/favicons',
-        html: 'test/test.html',
-        iconsPath: 'favicons'
-    },
-    settings: {
-        appName: 'Favicons',
-        appDescription: 'Favicon generator for Node.js',
-        developer: 'Hayden Bleasel',
-        developerURL: 'http://haydenbleasel.com',
-        background: '#27353f',
-        index: 'test/favicons.html',
-        url: 'http://haydenbleasel.com',
-        logging: true
-    }
-  };
-
-fav(config, function (error, metadata) {
-    'use strict';
-    console.log(error, metadata);
-});
-
-fs.readFile('test/logo.png', function (err, data) {
-    config = fav.getConfig(config);
-    config.data.favicon_generation.master_picture = { type: 'inline', content: data.toString('base64') };
-    fav.generateFaviconStream(config, function(error, result) {
-        console.log('stream callback: ', result.favicon_generation_result.result);
-    }).on('entry', function(entry) {
-        passThrough.write(new File({path: entry.path, contents: entry}));
-    });
-});
-
-var passThrough = through.obj({ highWaterMark: 50 }, function(obj, enc, cb) {
-    console.log(obj);
-    cb(null, obj);
+var favicons = require('./index');
+favicons('./test/logo.png', {
+    logging: true,
+    background: '#26353F'
+}, function () {
+    // if writing, make directory
+    console.log('done');
 });
