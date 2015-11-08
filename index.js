8509868b9694b2ba29b1142b01bcca29efff22f3
Hayden Bleasel <haydenbleasel@gmail.com>
2015-11-08 00:19:37 -0800

    Attempt separating icons and files

--- index.js
+++ index.js
@@ -11,15 +11,12 @@
         color = require('tinycolor2'),
         cheerio = require('cheerio'),
         colors = require('colors'),
+        config = require('loadobjects').sync('config');
 
-        // Configuration files
-        icons = require('./data/icons.json'),
-        defaults = require('./data/defaults.json');
-
-    module.exports = function (source, configuration, next) {
+    module.exports = function (source, parameters, next) {
 
         // 1. Take a set of icon parameters (default all to true)
-        var options = _.defaults(configuration || {}, defaults),
+        var options = _.defaults(parameters || {}, config.defaults),
             rgba = color(options.background).toRgb(),
             background = Jimp.rgbaToInt(rgba.r, rgba.g, rgba.b, rgba.a * 255);
 
@@ -33,32 +30,32 @@
             }
         }
 
-        function createFile(config, name, callback) {
-            print(name, 'Creating new file');
+        function createFile(properties, name, callback) {
+            print(name, 'Creating new file' + properties);
             return callback(null);
         }
 
-        function createImage(sourceset, config, name, callback) {
-            var minimum = Math.min(config.width, config.height),
+        function createImage(sourceset, properties, name, callback) {
+            var minimum = Math.min(properties.width, properties.height),
                 icon = _.min(sourceset, function (image) {
                     return image.size >= minimum;
                 }),
                 offset = {
-                    height: (config.height - minimum > 0 ? (config.height - minimum) / 2 : 0),
-                    width: (config.width - minimum > 0 ? (config.width - minimum) / 2 : 0)
+                    height: (properties.height - minimum > 0 ? (properties.height - minimum) / 2 : 0),
+                    width: (properties.width - minimum > 0 ? (properties.width - minimum) / 2 : 0)
                 },
-                jimp = new Jimp(config.width, config.height, background, function (error, canvas) {
+                jimp = new Jimp(properties.width, properties.height, background, function (error, canvas) {
                     if (error) {
                         return callback(error);
                     }
-                    print(name, 'Generated empty ' + config.width + 'x' + config.height + ' canvas with ' + background + ' background');
+                    print(name, 'Generated empty ' + properties.width + 'x' + properties.height + ' canvas with ' + background + ' background');
                     Jimp.read(icon.file).then(function (image) {
                         image.resize(minimum, Jimp.AUTO);
                         print(name, 'Resized ' + icon.size + 'x' + icon.size + ' image to ' + minimum + 'x' + minimum);
                         canvas.composite(image, offset.width, offset.height);
-                        print(name, 'Composited ' + minimum + 'x' + minimum + ' favicon on ' + config.width + 'x' + config.height + ' canvas');
+                        print(name, 'Composited ' + minimum + 'x' + minimum + ' favicon on ' + properties.width + 'x' + properties.height + ' canvas');
                         canvas.getBuffer(Jimp.MIME_PNG, function (error, buffer) {
-                            print(name, 'Collected image PNG buffer');
+                            print(name, 'Collected image PNG buffer from ' + jimp);
                             return callback(error, {
                                 name: name,
                                 buffer: buffer
@@ -79,17 +76,17 @@
             return callback(null, $.html());
         }
 
-        function createFavicon(sourceset, config, name, callback) {
+        function createFavicon(sourceset, properties, name, callback) {
             async.parallel([
                 function (callback) {
-                    createImage(sourceset, config, name, function (error, image) {
+                    createImage(sourceset, properties, name, function (error, image) {
                         print(name, 'Image has been created');
                         return callback(error, image);
                     });
                 },
                 function (callback) {
-                    if (config.html) {
-                        generateHTML(config.html, name, function (error, code) {
+                    if (properties.html) {
+                        generateHTML(properties.html, name, function (error, code) {
                             print(name, 'Generated HTML: ' + code);
                             return callback(error, code);
                         });
@@ -102,34 +99,55 @@
             });
         }
 
-        function createPlatform(sourceset, icons, callback) {
-            var images = [], files = [], html = [];
-            async.forEachOf(icons, function (config, name, callback) {
-                if (config.type === 'file') {
-                    createFile(config, name, function (file) {
-                        files.push(file);
-                        return callback(null);
-                    });
-                } else if (config.type === 'image') {
-                    createFavicon(sourceset, config, name, function (error, image, code) {
-                        images.push(image);
-                        html.push(code);
-                        return callback(error);
-                    });
-                } else {
-                    return callback('Something went wrong with icon configuration.');
-                }
+        function createFiles(platform, callback) {
+            var files = [];
+            async.forEachOf(config.files[platform], function (properties, name, callback) {
+                createFile(properties, name, function (file) {
+                    files.push(file);
+                    return callback(null);
+                });
             }, function (error) {
-                return callback(error, _.compact(images), _.compact(files), _.compact(html));
+                return callback(error, _.compact(files));
             });
         }
 
-        function createFavicons(sourceset, platforms, callback) {
+        function createFavicons(sourceset, platform, callback) {
+            var images = [], html = [];
+            async.forEachOf(config.icons[platform], function (properties, name, callback) {
+                createFavicon(sourceset, properties, name, function (error, image, code) {
+                    images.push(image);
+                    html.push(code);
+                    return callback(error);
+                });
+            }, function (error) {
+                return callback(error, _.compact(images), _.compact(html));
+            });
+        }
+
+        function createPlatform(sourceset, platform, callback) {
+            async.parallel([
+                function (callback) {
+                    createFavicons(sourceset, platform, function (images, html) {
+                        return callback(images, html);
+                    });
+                },
+                function (callback) {
+                    createFiles(platform, function (files) {
+                        return callback(files);
+                    });
+                }
+            ], function (error, results) {
+                console.log(results, 'resu;ts');
+                return callback(error, results[0], results[1]);
+            });
+        }
+
+        function generate(sourceset, platforms, callback) {
             var response = { images: [], files: [], html: [] };
             async.forEachOf(platforms, function (enabled, platform, callback) {
                 if (enabled) {
                     print(platform, 'Creating favicons for platform');
-                    createPlatform(sourceset, icons[platform], function (error, images, files, html) {
+                    createPlatform(sourceset, platform, function (error, images, files, html) {
                         response.images = response.images.concat(images);
                         response.files = response.files.concat(files);
                         response.html = response.html.concat(html);
@@ -161,7 +179,7 @@
             },
             function (sourceset, callback) {
                 print('Favicons:root', 'Creating favicons');
-                createFavicons(sourceset, options.icons, function (error, response) {
+                generate(sourceset, options.icons, function (error, response) {
                     print('Favicons:root', 'Returning response');
                     return callback(error, response);
                 });
