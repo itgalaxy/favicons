b21e5d59400c8b83366258569ef6718600f8ef91
Hayden Bleasel <haydenbleasel@gmail.com>
2015-11-22 02:16:18 -0800

    Updated test script

--- test.js
+++ test.js
@@ -1,46 +1,44 @@
-/*jslint node:true, nomen:true, stupid:true*/
-(function () {
+/* eslint no-sync: 0 */
+
+const favicons = require('./index'),
+    fs = require('fs'),
+    mkdirp = require('mkdirp');
+
+(() => {
 
     'use strict';
 
-    var favicons = require('./index'),
-        fs = require('fs'),
-        mkdirp = require('mkdirp')
-
     favicons('./test/logo.png', {
-        appName: "Favicons 4.0",
-        appDescription: "Testing suite for Favicons",
-        developerName: "Hayden Bleasel",
-        developerURL: "http://haydenbleasel.com/",
-        background: "#26353F",
-        path: "test/images/",
-        url: "http://haydenbleasel.com/",
-        version: "1.0",
+        appName: 'Favicons 4.0',
+        appDescription: 'Testing suite for Favicons',
+        developerName: 'Hayden Bleasel',
+        developerURL: 'http://haydenbleasel.com/',
+        background: '#26353F',
+        path: 'test/images/',
+        url: 'http://haydenbleasel.com/',
+        version: '1.0',
         logging: true,
         online: false
-    }, function callback (error, response) {
+    }, (error, response) => {
 
-        // error: any error that occurred in the process (string)
         if (error) {
             throw error;
         }
 
-        console.log('Images: ' + response.images);
-        console.log('Files: ' + response.files);
-        console.log('HTML: ' + response.html);
+        console.log(`Images: ${ response.images }`);
+        console.log(`Files: ${ response.files }`);
+        console.log(`HTML: ${ response.html }`);
 
         if (response.images) {
             mkdirp.sync('./test/images-offline/');
-            response.images.forEach(function (image) {
-                fs.writeFileSync('./test/images-offline/' + image.name, image.contents);
-            });
+            response.images.forEach((image) =>
+                fs.writeFileSync(`./test/images-offline/${ image.name }`, image.contents));
         }
 
         if (response.files) {
             mkdirp.sync('./test/files-offline/');
-            response.files.forEach(function (file) {
-                fs.writeFileSync('./test/files-offline/' + file.name, file.contents);
-            });
+            response.files.forEach((file) =>
+                fs.writeFileSync(`./test/files-offline/${ file.name }`, file.contents));
         }
 
         if (response.html) {
@@ -50,39 +48,36 @@
     });
 
     favicons('./test/logo.png', {
-        appName: "Favicons 4.0",
-        appDescription: "Testing suite for Favicons",
-        developerName: "Hayden Bleasel",
-        developerURL: "http://haydenbleasel.com/",
-        background: "#26353F",
-        path: "test/images/",
-        version: "1.0",
+        appName: 'Favicons 4.0',
+        appDescription: 'Testing suite for Favicons',
+        developerName: 'Hayden Bleasel',
+        developerURL: 'http://haydenbleasel.com/',
+        background: '#26353F',
+        path: 'test/images/',
+        version: '1.0',
         online: true
-    }, function callback (error, response) {
+    }, (error, response) => {
 
-        // error: any error that occurred in the process (string)
         if (error) {
             throw error;
         }
 
-        console.log('Images: ' + response.images);
-        console.log('Files: ' + response.files);
-        console.log('HTML: ' + response.html);
+        console.log(`Images: ${ response.images }`);
+        console.log(`Files: ${ response.files }`);
+        console.log(`HTML: ${ response.html }`);
 
         if (response.images) {
             mkdirp.sync('./test/images-online/');
-            response.images.forEach(function (image) {
-                fs.writeFileSync('./test/images-online/' + image.name, image.contents);
-            });
+            response.images.forEach((image) =>
+                fs.writeFileSync(`./test/images-online/${ image.name }`, image.contents));
         }
 
         if (response.files) {
             mkdirp.sync('./test/files-online/');
-            response.files.forEach(function (file) {
-                fs.writeFileSync('./test/files-online/' + file.name, file.contents);
-            });
+            response.files.forEach((file) =>
+                fs.writeFileSync(`./test/files-online/${ file.name }`, file.contents));
         }
 
     });
 
-}());
+})();
