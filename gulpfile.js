b0b856d1d5c1934e3d5e7d6b925cb669f17c73c6
Julian Grinblat <julian@dotcore.co.il>
2016-01-30 18:38:07 +0900

    Add gulp for building automation

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp   = require('gulp'),
+	gulpif = require('gulp-if'),
+    babel  = require('gulp-babel'),
+    rename = require("gulp-rename");
+
+gulp.task('build', () => {
+    return gulp.src(['index.js', 'helpers.js'])
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(gulpif(/^index\.js$/, rename('es5.js')))
+    .pipe(gulpif(/^helpers\.js$/, rename('helpers-es5.js')))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['build']);
