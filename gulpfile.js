1df5cf04d7c9be95a85666187df0ee0921ed63e5
Bruno Dutra <brunocodutra@gmail.com>
2018-03-28 17:21:24 +0200

    update dev dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 var gulp   = require('gulp'),
     babel  = require('gulp-babel');
 
-gulp.task('default', () => {
+gulp.task('default', () => Promise.all([
     gulp.src(['src/index.js', 'src/helpers.js'])
     .pipe(babel({
-        presets: ['es2015']
+        presets: [
+            ['env', {
+                targets: {
+                    node: "4"
+                },
+                exclude: [
+                    'transform-regenerator'
+                ]
+            }]
+        ]
     }))
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/')),
 
     gulp.src(['src/mask.png', 'src/overlay.png'])
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/')),
 
     gulp.src(['src/config/*'])
-    .pipe(gulp.dest('dist/config/'));
-});
+    .pipe(gulp.dest('dist/config/')),
+]));
