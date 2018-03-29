c5dd0bb0ac7b2d06439bd4e71d73959cfbcf7c95
Bruno Dutra <brunocodutra@gmail.com>
2018-03-29 19:32:19 +0200

    use ava's built in snapshot feature

--- test/default.test.js
+++ test/default.test.js
@@ -1,62 +1,22 @@
-const favicons = require('../src/'),
-    test = require('ava'),
-    fs = require('fs'),
-    path = require('path'),
-    mkdirp = require('mkdirp'),
-    dircompare = require('dir-compare');
+const favicons = require('../src');
+const test = require('ava');
+const path = require('path');
 
 test.cb('should generate the expected default result', t => {
     t.plan(1);
 
-    favicons(path.join(__dirname, 'logo.png'), {
-        appName: 'Favicons',
-        appDescription: 'Testing suite for Favicons',
-        developerName: 'Hayden Bleasel',
-        developerURL: 'http://haydenbleasel.com/',
-        background: '#26353F',
-        theme_color: 'aliceblue',
-        dir: 'ltr',
-        lang: 'en-US',
-        path: '',
-        url: 'http://haydenbleasel.com/',
-        display: 'browser',
-        orientation: 'landscape',
-        start_url: "/?homescreen=1",
-        version: '1.0',
-        logging: true,
-    }, (error, response) => {
+    favicons(path.join(__dirname, 'logo.png'), {}, (error, response) => {
 
         if (error) {
             throw error;
         }
 
-        const output = path.join(__dirname, 'output');
-        const fixtures = path.join(__dirname, 'fixtures');
+        const result = [...response.files, ...response.images].reduce(
+            (obj, {name, contents}) => Object.assign(obj, {[name]: contents}),
+            {'index.html': response.html}
+        );
 
-        mkdirp.sync(output);
-
-        if (response.images) {
-            response.images.forEach((image) =>
-                fs.writeFileSync(path.join(output, image.name), image.contents));
-        }
-
-        if (response.files) {
-            response.files.forEach((file) =>
-                fs.writeFileSync(path.join(output, file.name), file.contents));
-        }
-
-        if (response.html) {
-            fs.writeFileSync(path.join(output, 'index.html'), response.html.join('\n'));
-        }
-
-        dircompare.compare(output, fixtures, {compareContent: true}).then(diff => {
-            const failed = diff.diffSet
-                .filter(result => result.state !== 'equal')
-                .map(result => `${path.join(result.path1 || '', result.name1 + '')} ≠ ${path.join(result.path2 || '', result.name2 + '')}`);
-
-            t.deepEqual(failed, []);
-            t.end();
-        });
+        t.snapshot(result);
+        t.end();
     });
-
 });
--- test/stream.test.js
+++ test/stream.test.js
@@ -1,46 +1,18 @@
-const gulp = require('gulp'),
-    test = require('ava'),
-    gutil = require('gutil'),
-    path = require('path'),
-    favicons = require('../src/').stream,
-    dircompare = require('dir-compare');
+const favicons = require('../src').stream;
+const test = require('ava');
+const path = require('path');
+const gulp = require('gulp');
 
 test.cb('should provide stream interface', t => {
     t.plan(1);
 
-    const output = path.join(__dirname, 'stream');
-    const fixtures = path.join(__dirname, 'fixtures');
+    const result = {};
 
     gulp.src(path.join(__dirname, 'logo.png'))
-        .pipe(favicons({
-            appName: 'Favicons',
-            appDescription: 'Testing suite for Favicons',
-            developerName: 'Hayden Bleasel',
-            developerURL: 'http://haydenbleasel.com/',
-            background: '#26353F',
-            theme_color: 'aliceblue',
-            dir: 'ltr',
-            lang: 'en-US',
-            path: '',
-            url: 'http://haydenbleasel.com/',
-            display: 'browser',
-            orientation: 'landscape',
-            start_url: "/?homescreen=1",
-            version: '1.0',
-            logging: true,
-            html: 'index.html',
-            pipeHTML: true,
-            replace: false
-        }))
-        .on('error', gutil.log)
-        .pipe(gulp.dest(output));
-
-    dircompare.compare(output, fixtures, {compareContent: true}).then(diff => {
-        const failed = diff.diffSet
-            .filter(result => result.state !== 'equal')
-            .map(result => `${path.join(result.path1 || '', result.name1 + '')} ≠ ${path.join(result.path2 || '', result.name2 + '')}`);
-
-        t.deepEqual(failed, []);
-        t.end();
-    });
+        .pipe(favicons({ html: 'index.html', pipeHTML: true }))
+        .on('data', chunk => result[chunk.path] = chunk.contents)
+        .on('end', () => {
+            t.snapshot(result);
+            t.end();
+        });
 });
