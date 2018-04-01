bc46a52627954d6f304c1469ec90f64800313170
Bruno Dutra <brunocodutra@gmail.com>
2018-04-01 13:48:14 +0200

    move test fixtures to a subfolder

--- test/util.js
+++ test/util.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
-module.exports.logo_png = path.resolve(__dirname, "logo.png");
-module.exports.logo_svg = path.resolve(__dirname, "logo.svg");
+module.exports.logo_png = path.resolve(__dirname, "fixtures", "logo.png");
+module.exports.logo_svg = path.resolve(__dirname, "fixtures", "logo.svg");
 
 module.exports.normalize = ({ files, images, html }) =>
   [...files, ...images].reduce(
