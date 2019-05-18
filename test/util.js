0750f24ef39907c41b3a680d65839e9f5e2dcb9c
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-05-18 22:21:30 +0300

    Implement test for scaling small SVGs to larger resolutions

--- test/util.js
+++ test/util.js
@@ -2,6 +2,11 @@ const path = require("path");
 
 module.exports.logo_png = path.resolve(__dirname, "fixtures", "logo.png");
 module.exports.logo_svg = path.resolve(__dirname, "fixtures", "logo.svg");
+module.exports.logo_small_svg = path.resolve(
+  __dirname,
+  "fixtures",
+  "logo_small.svg"
+);
 module.exports.pixel_art = path.resolve(__dirname, "fixtures", "pixel_art.png");
 
 module.exports.normalize = ({ files, images, html }) =>
