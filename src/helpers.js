eb5eb6dbc431c00a6d700e8ffcee50349d98af7c
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-05-18 22:18:38 +0300

    Scale the original SVG in order to render it sharply
    In order to render the lower resolution SVGs properly with sharp,
    the original SVG files need to first have large enough width and
    height attributes.
    
    Fix #264

--- src/helpers.js
+++ src/helpers.js
@@ -8,6 +8,7 @@ const jsonxml = require("jsontoxml");
 const sizeOf = require("image-size");
 const Jimp = require("jimp");
 const sharp = require("sharp");
+const xml2js = require("xml2js");
 const PLATFORM_OPTIONS = require("./config/platform-options.json");
 
 module.exports = function(options) {
@@ -37,6 +38,63 @@ module.exports = function(options) {
 
     return Jimp.rgbaToInt(r, g, b, a * 255);
   }
+
+  // sharp renders the SVG in its source width and height with 72 DPI which can
+  // cause a blurry result in case the source SVG is defined in lower size than
+  // the target size. To avoid this, resize the source SVG to the needed size
+  // before passing it to sharp by increasing its width and/or height
+  // attributes.
+  //
+  // Currently it seems this won't be fixed in sharp, so we need a workaround:
+  // https://github.com/lovell/sharp/issues/729#issuecomment-284708688
+  //
+  // They suggest setting the image density to a "resized" density based on the
+  // target render size but this does not seem to work with favicons and may
+  // cause other errors with "unnecessarily high" image density values.
+  //
+  // For further information, see:
+  // https://github.com/itgalaxy/favicons/issues/264
+  const svgtool = {
+    ensureSize(svgSource, width, height) {
+      let svgWidth = svgSource.size.width;
+      let svgHeight = svgSource.size.height;
+
+      if (svgWidth >= width && svgHeight >= height) {
+        // If the base SVG is large enough, it does not need to be modified.
+        return Promise.resolve(svgSource.file);
+      } else if (width > height) {
+        svgHeight = Math.round(svgHeight * (width / svgWidth));
+        svgWidth = width;
+      } else {
+        // width <= height
+        svgWidth = Math.round(svgWidth * (height / svgHeight));
+        svgHeight = height;
+      }
+
+      // Modify the source SVG's width and height attributes for sharp to render
+      // it correctly.
+      log("svgtool:ensureSize", `Resizing SVG to ${svgWidth}x${svgHeight}`);
+      return this.resize(svgSource.file, svgWidth, svgHeight);
+    },
+
+    resize(svgFile, width, height) {
+      return new Promise((resolve, reject) => {
+        xml2js.parseString(svgFile, (err, xmlDoc) => {
+          if (err) {
+            return reject(err);
+          }
+
+          xmlDoc.svg.$.width = width;
+          xmlDoc.svg.$.height = height;
+
+          const builder = new xml2js.Builder();
+          const modifiedSvg = builder.buildObject(xmlDoc);
+
+          resolve(Buffer.from(modifiedSvg));
+        });
+      });
+    }
+  };
 
   return {
     General: {
@@ -214,14 +272,18 @@ module.exports = function(options) {
           const background = { r: 0, g: 0, b: 0, alpha: 0 };
 
           log("Image:render", `Rendering SVG to ${width}x${height}`);
-          promise = sharp(svgSource.file)
-            .resize({
-              background,
-              width,
-              height,
-              fit: sharp.fit.contain
-            })
-            .toBuffer()
+          promise = svgtool
+            .ensureSize(svgSource, width, height)
+            .then(svgBuffer =>
+              sharp(svgBuffer)
+                .resize({
+                  background,
+                  width,
+                  height,
+                  fit: sharp.fit.contain
+                })
+                .toBuffer()
+            )
             .then(Jimp.read);
         } else {
           const sideSize = Math.max(width, height);
