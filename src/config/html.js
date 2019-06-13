65b73aabed574125436ac948c130109e0c905327
Reto Casserini <44959292+neucassi@users.noreply.github.com>
2019-06-13 13:30:04 +0700

    Update html.js

--- src/config/html.js
+++ src/config/html.js
@@ -35,7 +35,8 @@ module.exports = {
     ({ relative }) => `<link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (orientation: landscape) and (-webkit-device-pixel-ratio: 1)" href="${relative("apple-touch-startup-image-748x1024.png")}">`,
     ({ relative }) => `<link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 1)" href="${relative("apple-touch-startup-image-768x1004.png")}">`,
     ({ relative }) => `<link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (orientation: landscape) and (-webkit-device-pixel-ratio: 2)" href="${relative("apple-touch-startup-image-1496x2048.png")}">`,
-    ({ relative }) => `<link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)" href="${relative("apple-touch-startup-image-1536x2008.png")}">`
+    ({ relative }) => `<link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)" href="${relative("apple-touch-startup-image-1536x2008.png")}">`,
+    ({ relative }) => `<link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 3)" href="${relative("apple-touch-startup-image-1125x2436.png")}">`
   ],
   coast: [
     ({ relative }) => `<link rel="icon" type="image/png" sizes="228x228" href="${relative("coast-228x228.png")}">`
