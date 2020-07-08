4f5a78d89d3b691ba81e79cf4a53e5ac24957bf9
Voltra <ludwig.guerin.98@gmx.fr>
2020-07-08 15:30:37 +0200

    Add tests for favicon restrictions

--- test/restrict.test.js
+++ test/restrict.test.js
@@ -1,22 +1,30 @@
 const favicons = require("../src");
 const test = require("ava");
 
-const { snapshotManager } = require("ava/lib/concordance-options");
-const { factory } = require("concordance-comparator");
+// const { snapshotManager } = require("ava/lib/concordance-options");
+// const { factory } = require("concordance-comparator");
 
 const { logo_png } = require("./util");
-const { Image } = require("./Image");
+// const { Image } = require("./Image");
 
-snapshotManager.plugins.push(factory(Image, v => new Image(v[0], v[1])));
+// snapshotManager.plugins.push(factory(Image, v => new Image(v[0], v[1])));
 
 test("should allow to restrict the icons to generate", async t => {
+  // eslint-disable-next-line no-magic-numbers
   t.plan(1);
 
-  const { files, images, html } = await favicons(logo_png, {
-    icons: { favicons: ["favicon-32x32.png"] }
+  const { images } = await favicons(logo_png, {
+    icons: {
+      favicons: ["favicon-32x32.png"],
+      android: false,
+      appleIcon: false,
+      appleStartup: false,
+      coast: false,
+      firefox: false,
+      windows: false,
+      yandex: false
+    }
   });
 
-  t.deepEqual(files, []);
   t.deepEqual(images.length, 1);
-  t.deepEqual(html, []);
 });
