170ad1a215b13633197ef01352c3df5b76438b50
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2021-05-11 17:36:18 +0300

    chore(deps): update (#331)

--- readme.md
+++ readme.md
@@ -26,58 +26,58 @@ Favicons generates its icons locally using pure Javascript with no external depe
 Please note: Favicons is tested on Node 10.13 and above.
 
 ```js
-var favicons = require('favicons'),
-    source = 'test/logo.png',                     // Source image(s). `string`, `buffer` or array of `string`
-    configuration = {
-        path: "/",                                // Path for overriding default icons path. `string`
-        appName: null,                            // Your application's name. `string`
-        appShortName: null,                       // Your application's short_name. `string`. Optional. If not set, appName will be used
-        appDescription: null,                     // Your application's description. `string`
-        developerName: null,                      // Your (or your developer's) name. `string`
-        developerURL: null,                       // Your (or your developer's) URL. `string`
-        dir: "auto",                              // Primary text direction for name, short_name, and description
-        lang: "en-US",                            // Primary language for name and short_name
-        background: "#fff",                       // Background colour for flattened icons. `string`
-        theme_color: "#fff",                      // Theme color user for example in Android's task switcher. `string`
-        appleStatusBarStyle: "black-translucent", // Style for Apple status bar: "black-translucent", "default", "black". `string`
-        display: "standalone",                    // Preferred display mode: "fullscreen", "standalone", "minimal-ui" or "browser". `string`
-        orientation: "any",                       // Default orientation: "any", "natural", "portrait" or "landscape". `string`
-        scope: "/",                               // set of URLs that the browser considers within your app
-        start_url: "/?homescreen=1",              // Start URL when launching the application from a device. `string`
-        version: "1.0",                           // Your application's version string. `string`
-        logging: false,                           // Print logs to console? `boolean`
-        pixel_art: false,                         // Keeps pixels "sharp" when scaling up, for pixel art.  Only supported in offline mode.
-        loadManifestWithCredentials: false,       // Browsers don't send cookies when fetching a manifest, enable this to fix that. `boolean`
-        icons: {
-            // Platform Options:
-            // - offset - offset in percentage
-            // - background:
-            //   * false - use default
-            //   * true - force use default, e.g. set background for Android icons
-            //   * color - set background for the specified icons
-            //   * mask - apply mask in order to create circle icon (applied by default for firefox). `boolean`
-            //   * overlayGlow - apply glow effect after mask has been applied (applied by default for firefox). `boolean`
-            //   * overlayShadow - apply drop shadow after mask has been applied .`boolean`
-            //
-            android: true,              // Create Android homescreen icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
-            appleIcon: true,            // Create Apple touch icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
-            appleStartup: true,         // Create Apple startup images. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
-            coast: true,                // Create Opera Coast icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
-            favicons: true,             // Create regular favicons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
-            firefox: true,              // Create Firefox OS icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
-            windows: true,              // Create Windows 8 tile icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
-            yandex: true                // Create Yandex browser icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
-        }
+var favicons = require("favicons"),
+  source = "test/logo.png", // Source image(s). `string`, `buffer` or array of `string`
+  configuration = {
+    path: "/", // Path for overriding default icons path. `string`
+    appName: null, // Your application's name. `string`
+    appShortName: null, // Your application's short_name. `string`. Optional. If not set, appName will be used
+    appDescription: null, // Your application's description. `string`
+    developerName: null, // Your (or your developer's) name. `string`
+    developerURL: null, // Your (or your developer's) URL. `string`
+    dir: "auto", // Primary text direction for name, short_name, and description
+    lang: "en-US", // Primary language for name and short_name
+    background: "#fff", // Background colour for flattened icons. `string`
+    theme_color: "#fff", // Theme color user for example in Android's task switcher. `string`
+    appleStatusBarStyle: "black-translucent", // Style for Apple status bar: "black-translucent", "default", "black". `string`
+    display: "standalone", // Preferred display mode: "fullscreen", "standalone", "minimal-ui" or "browser". `string`
+    orientation: "any", // Default orientation: "any", "natural", "portrait" or "landscape". `string`
+    scope: "/", // set of URLs that the browser considers within your app
+    start_url: "/?homescreen=1", // Start URL when launching the application from a device. `string`
+    version: "1.0", // Your application's version string. `string`
+    logging: false, // Print logs to console? `boolean`
+    pixel_art: false, // Keeps pixels "sharp" when scaling up, for pixel art.  Only supported in offline mode.
+    loadManifestWithCredentials: false, // Browsers don't send cookies when fetching a manifest, enable this to fix that. `boolean`
+    icons: {
+      // Platform Options:
+      // - offset - offset in percentage
+      // - background:
+      //   * false - use default
+      //   * true - force use default, e.g. set background for Android icons
+      //   * color - set background for the specified icons
+      //   * mask - apply mask in order to create circle icon (applied by default for firefox). `boolean`
+      //   * overlayGlow - apply glow effect after mask has been applied (applied by default for firefox). `boolean`
+      //   * overlayShadow - apply drop shadow after mask has been applied .`boolean`
+      //
+      android: true, // Create Android homescreen icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+      appleIcon: true, // Create Apple touch icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+      appleStartup: true, // Create Apple startup images. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+      coast: true, // Create Opera Coast icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+      favicons: true, // Create regular favicons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+      firefox: true, // Create Firefox OS icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+      windows: true, // Create Windows 8 tile icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+      yandex: true, // Create Yandex browser icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
     },
-    callback = function (error, response) {
-        if (error) {
-            console.log(error.message); // Error description e.g. "An unknown error has occurred"
-            return;
-        }
-        console.log(response.images);   // Array of { name: string, contents: <buffer> }
-        console.log(response.files);    // Array of { name: string, contents: <string> }
-        console.log(response.html);     // Array of strings (html elements)
-    };
+  },
+  callback = function (error, response) {
+    if (error) {
+      console.log(error.message); // Error description e.g. "An unknown error has occurred"
+      return;
+    }
+    console.log(response.images); // Array of { name: string, contents: <buffer> }
+    console.log(response.files); // Array of { name: string, contents: <string> }
+    console.log(response.html); // Array of strings (html elements)
+  };
 
 favicons(source, configuration, callback);
 ```
@@ -167,7 +167,7 @@ The default sources are as follow (groupped by platform):
 You can programmatically access Favicons configuration (icon filenames, HTML, manifest files, etc) with:
 
 ```js
-var config = require('favicons').config;
+var config = require("favicons").config;
 ```
 
 ### Gulp
@@ -176,10 +176,13 @@ To use Favicons with Gulp, you can either use the [gulp-plugin](https://github.c
 
 ```js
 var favicons = require("favicons").stream,
-    log = require("fancy-log");
+  log = require("fancy-log");
 
 gulp.task("default", function () {
-    return gulp.src("logo.png").pipe(favicons({
+  return gulp
+    .src("logo.png")
+    .pipe(
+      favicons({
         appName: "My App",
         appShortName: "App",
         appDescription: "This is my application",
@@ -196,8 +199,9 @@ gulp.task("default", function () {
         logging: false,
         html: "index.html",
         pipeHTML: true,
-        replace: true
-    }))
+        replace: true,
+      })
+    )
     .on("error", log)
     .pipe(gulp.dest("./"));
 });
--- src/config/html.js
+++ src/config/html.js
@@ -1,243 +1,223 @@
 /* eslint-disable */
 
-const appleIconSizes = [
-  57,
-  60,
-  72,
-  76,
-  114,
-  120,
-  144,
-  152,
-  167,
-  180,
-  1024
-];
+const appleIconSizes = [57, 60, 72, 76, 114, 120, 144, 152, 167, 180, 1024];
 
 const appleStartupItems = [
-    //
-    // Device              Portrait size      Landscape size     Screen size        Pixel ratio
-    // iPhone SE            640px × 1136px    1136px ×  640px     320px ×  568px    2
-    // iPhone 8             750px × 1334px    1334px ×  750px     375px ×  667px    2
-    // iPhone 7             750px × 1334px    1334px ×  750px     375px ×  667px    2
-    // iPhone 6s            750px × 1334px    1334px ×  750px     375px ×  667px    2
-    // iPhone XR            828px × 1792px    1792px ×  828px     414px ×  896px    2
-    // iPhone XS           1125px × 2436px    2436px × 1125px     375px ×  812px    3
-    // iPhone X            1125px × 2436px    2436px × 1125px     375px ×  812px    3
-    // iPhone 8 Plus       1242px × 2208px    2208px × 1242px     414px ×  736px    3
-    // iPhone 7 Plus       1242px × 2208px    2208px × 1242px     414px ×  736px    3
-    // iPhone 6s Plus      1242px × 2208px    2208px × 1242px     414px ×  736px    3
-    // iPhone XS Max       1242px × 2688px    2688px × 1242px     414px ×  896px    3
-    // 9.7" iPad           1536px × 2048px    2048px × 1536px     768px × 1024px    2
-    // 10.2" iPad          1620px × 2160px    2160px x 1620px     810px × 1080px    2
-    // 7.9" iPad mini 4    1536px × 2048px    2048px × 1536px     768px × 1024px    2
-    // 10.5" iPad Pro      1668px × 2224px    2224px × 1668px     834px × 1112px    2
-    // 11" iPad Pro        1668px × 2388px    2388px × 1668px     834px × 1194px    2
-    // 12.9" iPad Pro      2048px × 2732px    2732px × 2048px    1024px × 1366px    2
-    {
-      dwidth: 320,
-      dheight: 568,
-      pixelRatio: 2,
-      orientation: "portrait",
-      width: 640,
-      height: 1136,
-    },
-    {
-      dwidth: 375,
-      dheight: 667,
-      pixelRatio: 2,
-      orientation: "portrait",
-      width: 750,
-      height: 1334,
-    },
-    {
-      dwidth: 414,
-      dheight: 896,
-      pixelRatio: 2,
-      orientation: "portrait",
-      width: 828,
-      height: 1792,
-    },
-    {
-      dwidth: 375,
-      dheight: 812,
-      pixelRatio: 3,
-      orientation: "portrait",
-      width: 1125,
-      height: 2436,
-    },
-    {
-      dwidth: 414,
-      dheight: 736,
-      pixelRatio: 3,
-      orientation: "portrait",
-      width: 1242,
-      height: 2208,
-    },
-    {
-      dwidth: 414,
-      dheight: 896,
-      pixelRatio: 3,
-      orientation: "portrait",
-      width: 1242,
-      height: 2688,
-    },
-    {
-      dwidth: 768,
-      dheight: 1024,
-      pixelRatio: 2,
-      orientation: "portrait",
-      width: 1536,
-      height: 2048,
-    },
-    {
-      dwidth: 834,
-      dheight: 1112,
-      pixelRatio: 2,
-      orientation: "portrait",
-      width: 1668,
-      height: 2224,
-    },
-    {
-      dwidth: 834,
-      dheight: 1194,
-      pixelRatio: 2,
-      orientation: "portrait",
-      width: 1668,
-      height: 2388,
-    },
-    {
-      dwidth: 1024,
-      dheight: 1366,
-      pixelRatio: 2,
-      orientation: "portrait",
-      width: 2048,
-      height: 2732,
-    },
-    {
-      dwidth: 810,
-      dheight: 1080,
-      pixelRatio: 2,
-      orientation: "portrait",
-      width: 1620,
-      height: 2160,
-    },
+  //
+  // Device              Portrait size      Landscape size     Screen size        Pixel ratio
+  // iPhone SE            640px × 1136px    1136px ×  640px     320px ×  568px    2
+  // iPhone 8             750px × 1334px    1334px ×  750px     375px ×  667px    2
+  // iPhone 7             750px × 1334px    1334px ×  750px     375px ×  667px    2
+  // iPhone 6s            750px × 1334px    1334px ×  750px     375px ×  667px    2
+  // iPhone XR            828px × 1792px    1792px ×  828px     414px ×  896px    2
+  // iPhone XS           1125px × 2436px    2436px × 1125px     375px ×  812px    3
+  // iPhone X            1125px × 2436px    2436px × 1125px     375px ×  812px    3
+  // iPhone 8 Plus       1242px × 2208px    2208px × 1242px     414px ×  736px    3
+  // iPhone 7 Plus       1242px × 2208px    2208px × 1242px     414px ×  736px    3
+  // iPhone 6s Plus      1242px × 2208px    2208px × 1242px     414px ×  736px    3
+  // iPhone XS Max       1242px × 2688px    2688px × 1242px     414px ×  896px    3
+  // 9.7" iPad           1536px × 2048px    2048px × 1536px     768px × 1024px    2
+  // 10.2" iPad          1620px × 2160px    2160px x 1620px     810px × 1080px    2
+  // 7.9" iPad mini 4    1536px × 2048px    2048px × 1536px     768px × 1024px    2
+  // 10.5" iPad Pro      1668px × 2224px    2224px × 1668px     834px × 1112px    2
+  // 11" iPad Pro        1668px × 2388px    2388px × 1668px     834px × 1194px    2
+  // 12.9" iPad Pro      2048px × 2732px    2732px × 2048px    1024px × 1366px    2
+  {
+    dwidth: 320,
+    dheight: 568,
+    pixelRatio: 2,
+    orientation: "portrait",
+    width: 640,
+    height: 1136,
+  },
+  {
+    dwidth: 375,
+    dheight: 667,
+    pixelRatio: 2,
+    orientation: "portrait",
+    width: 750,
+    height: 1334,
+  },
+  {
+    dwidth: 414,
+    dheight: 896,
+    pixelRatio: 2,
+    orientation: "portrait",
+    width: 828,
+    height: 1792,
+  },
+  {
+    dwidth: 375,
+    dheight: 812,
+    pixelRatio: 3,
+    orientation: "portrait",
+    width: 1125,
+    height: 2436,
+  },
+  {
+    dwidth: 414,
+    dheight: 736,
+    pixelRatio: 3,
+    orientation: "portrait",
+    width: 1242,
+    height: 2208,
+  },
+  {
+    dwidth: 414,
+    dheight: 896,
+    pixelRatio: 3,
+    orientation: "portrait",
+    width: 1242,
+    height: 2688,
+  },
+  {
+    dwidth: 768,
+    dheight: 1024,
+    pixelRatio: 2,
+    orientation: "portrait",
+    width: 1536,
+    height: 2048,
+  },
+  {
+    dwidth: 834,
+    dheight: 1112,
+    pixelRatio: 2,
+    orientation: "portrait",
+    width: 1668,
+    height: 2224,
+  },
+  {
+    dwidth: 834,
+    dheight: 1194,
+    pixelRatio: 2,
+    orientation: "portrait",
+    width: 1668,
+    height: 2388,
+  },
+  {
+    dwidth: 1024,
+    dheight: 1366,
+    pixelRatio: 2,
+    orientation: "portrait",
+    width: 2048,
+    height: 2732,
+  },
+  {
+    dwidth: 810,
+    dheight: 1080,
+    pixelRatio: 2,
+    orientation: "portrait",
+    width: 1620,
+    height: 2160,
+  },
 
-
-    {
-      dwidth: 320,
-      dheight: 568,
-      pixelRatio: 2,
-      orientation: "landscape",
-      width: 1136,
-      height: 640,
-    },
-    {
-      dwidth: 375,
-      dheight: 667,
-      pixelRatio: 2,
-      orientation: "landscape",
-      width: 1334,
-      height: 750,
-    },
-    {
-      dwidth: 414,
-      dheight: 896,
-      pixelRatio: 2,
-      orientation: "landscape",
-      width: 1792,
-      height: 828,
-    },
-    {
-      dwidth: 375,
-      dheight: 812,
-      pixelRatio: 3,
-      orientation: "landscape",
-      width: 2436,
-      height: 1125,
-    },
-    {
-      dwidth: 414,
-      dheight: 736,
-      pixelRatio: 3,
-      orientation: "landscape",
-      width: 2208,
-      height: 1242,
-    },
-    {
-      dwidth: 414,
-      dheight: 896,
-      pixelRatio: 3,
-      orientation: "landscape",
-      width: 2688,
-      height: 1242,
-    },
-    {
-      dwidth: 768,
-      dheight: 1024,
-      pixelRatio: 2,
-      orientation: "landscape",
-      width: 2048,
-      height: 1536,
-    },
-    {
-      dwidth: 834,
-      dheight: 1112,
-      pixelRatio: 2,
-      orientation: "landscape",
-      width: 2224,
-      height: 1668,
-    },
-    {
-      dwidth: 834,
-      dheight: 1194,
-      pixelRatio: 2,
-      orientation: "landscape",
-      width: 2388,
-      height: 1668,
-    },
-    {
-      dwidth: 1024,
-      dheight: 1366,
-      pixelRatio: 2,
-      orientation: "landscape",
-      width: 2732,
-      height: 2048,
-    },
-    {
-      dwidth: 810,
-      dheight: 1080,
-      pixelRatio: 2,
-      orientation: "landscape",
-      width: 2160,
-      height: 1620,
-    },
+  {
+    dwidth: 320,
+    dheight: 568,
+    pixelRatio: 2,
+    orientation: "landscape",
+    width: 1136,
+    height: 640,
+  },
+  {
+    dwidth: 375,
+    dheight: 667,
+    pixelRatio: 2,
+    orientation: "landscape",
+    width: 1334,
+    height: 750,
+  },
+  {
+    dwidth: 414,
+    dheight: 896,
+    pixelRatio: 2,
+    orientation: "landscape",
+    width: 1792,
+    height: 828,
+  },
+  {
+    dwidth: 375,
+    dheight: 812,
+    pixelRatio: 3,
+    orientation: "landscape",
+    width: 2436,
+    height: 1125,
+  },
+  {
+    dwidth: 414,
+    dheight: 736,
+    pixelRatio: 3,
+    orientation: "landscape",
+    width: 2208,
+    height: 1242,
+  },
+  {
+    dwidth: 414,
+    dheight: 896,
+    pixelRatio: 3,
+    orientation: "landscape",
+    width: 2688,
+    height: 1242,
+  },
+  {
+    dwidth: 768,
+    dheight: 1024,
+    pixelRatio: 2,
+    orientation: "landscape",
+    width: 2048,
+    height: 1536,
+  },
+  {
+    dwidth: 834,
+    dheight: 1112,
+    pixelRatio: 2,
+    orientation: "landscape",
+    width: 2224,
+    height: 1668,
+  },
+  {
+    dwidth: 834,
+    dheight: 1194,
+    pixelRatio: 2,
+    orientation: "landscape",
+    width: 2388,
+    height: 1668,
+  },
+  {
+    dwidth: 1024,
+    dheight: 1366,
+    pixelRatio: 2,
+    orientation: "landscape",
+    width: 2732,
+    height: 2048,
+  },
+  {
+    dwidth: 810,
+    dheight: 1080,
+    pixelRatio: 2,
+    orientation: "landscape",
+    width: 2160,
+    height: 1620,
+  },
 ];
 
-const coastSizes = [
-  228,
-];
+const coastSizes = [228];
 
-const faviconSizes = [
-  16,
-  32,
-  48,
-];
+const faviconSizes = [16, 32, 48];
 
 function hasAll(arr) {
   return function (icons) {
-    if (Array.isArray(icons)) return arr.every(item => icons.includes(item));
+    if (Array.isArray(icons)) return arr.every((item) => icons.includes(item));
     return icons;
   };
 }
 
-function hasAny(arr){
-	return function(icons){
-		if(Array.isArray(icons))
-			return arr.some(item => icons.include(item));
-		
-		return icons;
-	}
+function hasAny(arr) {
+  return function (icons) {
+    if (Array.isArray(icons)) return arr.some((item) => icons.include(item));
+
+    return icons;
+  };
 }
 
 function ctxHasIcons(icons, icon) {
@@ -245,13 +225,21 @@ function ctxHasIcons(icons, icon) {
   return icons;
 }
 
-const allAppleIcons = hasAll(appleIconSizes.map(size => `apple-touch-icon-${size}x${size}.png`));
-const anyAppleIcon = hasAny(appleIconSizes.map(size => `apple-touch-icon-${size}x${size}.png`));
+const allAppleIcons = hasAll(
+  appleIconSizes.map((size) => `apple-touch-icon-${size}x${size}.png`)
+);
+const anyAppleIcon = hasAny(
+  appleIconSizes.map((size) => `apple-touch-icon-${size}x${size}.png`)
+);
 
 function appleIconGen(size, { relative, icons }) {
   const iconName = `apple-touch-icon-${size}x${size}.png`;
 
-  return !ctxHasIcons(icons.appleIcon, iconName) ? "" : `<link rel="apple-touch-icon" sizes="${size}x${size}" href="${relative(iconName)}">`;
+  return !ctxHasIcons(icons.appleIcon, iconName)
+    ? ""
+    : `<link rel="apple-touch-icon" sizes="${size}x${size}" href="${relative(
+        iconName
+      )}">`;
 }
 
 function appleStartupGen(
@@ -260,18 +248,30 @@ function appleStartupGen(
 ) {
   const iconName = `apple-touch-startup-image-${width}x${height}.png`;
 
-  return !ctxHasIcons(icons.appleStartup, iconName) ? "" : `<link rel="apple-touch-startup-image" media="(device-width: ${dwidth}px) and (device-height: ${dheight}px) and (-webkit-device-pixel-ratio: ${pixelRatio}) and (orientation: ${orientation})" href="${relative(iconName)}">`;
+  return !ctxHasIcons(icons.appleStartup, iconName)
+    ? ""
+    : `<link rel="apple-touch-startup-image" media="(device-width: ${dwidth}px) and (device-height: ${dheight}px) and (-webkit-device-pixel-ratio: ${pixelRatio}) and (orientation: ${orientation})" href="${relative(
+        iconName
+      )}">`;
 }
 
 function coastGen(size, { relative, icons }) {
   const iconName = `coast-${size}x${size}.png`;
 
-  return !ctxHasIcons(icons.coast, iconName) ? "" : `<link rel="icon" type="image/png" sizes="${size}x${size}" href="${relative(iconName)}">`;
+  return !ctxHasIcons(icons.coast, iconName)
+    ? ""
+    : `<link rel="icon" type="image/png" sizes="${size}x${size}" href="${relative(
+        iconName
+      )}">`;
 }
 
-function faviconGen(size, { relative, icons }){
+function faviconGen(size, { relative, icons }) {
   const iconName = `favicon-${size}x${size}.png`;
-  return !ctxHasIcons(icons.favicons, iconName) ? "" : `<link rel="icon" type="image/png" sizes="${size}x${size}" href="${relative(iconName)}">`;
+  return !ctxHasIcons(icons.favicons, iconName)
+    ? ""
+    : `<link rel="icon" type="image/png" sizes="${size}x${size}" href="${relative(
+        iconName
+      )}">`;
 }
 
 // prettier-ignore
