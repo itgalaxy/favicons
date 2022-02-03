c8be5d48ec9c1a4526202cf7a848d4183636bef7
Christian Moritz <chrmoritz@gmail.com>
2022-02-03 23:36:54 +0100

    add shortcuts test

---
+++ test/manifestShortcuts.test.js
@@ -0,0 +1,36 @@
+import favicons from "..";
+import * as fs from "fs";
+import { logo_png, logo_svg } from "./util";
+
+test("manifest should support shortcuts", async () => {
+  expect.assertions(1);
+
+  const result = await favicons(logo_png, {
+    shortcuts: [
+      {
+        name: "View your Inbox",
+        short_name: "inbox",
+        description: "View your inbox messages",
+        url: "/inbox",
+        icon: [logo_png, fs.readFileSync(logo_png)], // eslint-disable-line no-sync
+      },
+      {
+        name: "Picture Gallery",
+        short_name: "pictures",
+        description: "View your own Picture Gallery",
+        url: "/pictures",
+        icon: logo_svg,
+      },
+    ],
+    icons: {
+      android: true,
+      appleIcon: false,
+      appleStartup: false,
+      favicons: false,
+      windows: false,
+      yandex: false,
+    },
+  });
+
+  await expect(result).toMatchFaviconsSnapshot();
+});
